96c9cbbee305469beb908b9cc5ea3cb9c10ab1a2
Eduardo Matos <luizmatosedu@gmail.com>
2021-10-15 18:43:57 -0300

    :open_file_folder: Adicionado upload de avatar de usuário

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
+
+import ensureAuthenticated from '../middleware/ensureAuthenticated'
 import CreateUserService from '../services/CreateUserService'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
 
 const usersRouter = Router()
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -17,5 +23,26 @@ usersRouter.post('/', async (req, res) => {
     return res.json({ error: 'Server error' })
   }
 })
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService()
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file?.filename,
+      })
+      return res.json(user)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ error: err.message })
+      }
+      return res.json({ error: 'Server error' })
+    }
+  },
+)
 
 export default usersRouter
