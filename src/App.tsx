0ae8e8e14e0495c91211f3829ddb6584add09ef4
Eduardo Matos <luizmatosedu@gmail.com>
2021-10-26 14:53:09 -0300

    Adicionado styled component no projeto Finalização da estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import React from 'react'
+import { FC } from 'react'
 
-function App() {
+import GlobalStyle from './styles/global'
+
+const App: FC = function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
   )
 }
 
